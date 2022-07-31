53ddf70ebf29d4500346a77f4ecaab2b191334fd
HishamGhander <hisham011232@ci.suez.edu.eg>
2022-07-31 10:16:34 +0200

    intial commit springboot dynamic datasource routing

---
+++ src/test/java/com/allegiancemd/SpringBootDatasourceRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allegiancemd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatasourceRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
