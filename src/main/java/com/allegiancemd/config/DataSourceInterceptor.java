9d4de4f51c7382219e96802953174940dbde369a
HishamGhander <hisham011232@ci.suez.edu.eg>
2022-07-31 12:18:25 +0200

    used slf4j and some logs

--- src/main/java/com/allegiancemd/config/DataSourceInterceptor.java
+++ src/main/java/com/allegiancemd/config/DataSourceInterceptor.java
@@ -1,22 +1,25 @@
 package com.allegiancemd.config;
 
 import com.allegiancemd.constant.DatabaseEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @Component
 public class DataSourceInterceptor extends HandlerInterceptorAdapter {
-
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws Exception {
+        log.info(request.getRequestURI());
 
-        String branch = request.getHeader("database");
-        if (DatabaseEnum.ALLEGIANCEMD1.toString().equalsIgnoreCase(branch))
+        String database = request.getHeader("database");
+        log.info(database);
+        if (DatabaseEnum.ALLEGIANCEMD1.toString().equalsIgnoreCase(database))
             DatabaseContextHolder.setDatabaseContext(DatabaseEnum.ALLEGIANCEMD1);
         else
             DatabaseContextHolder.setDatabaseContext(DatabaseEnum.ALLEGIANCEMD2);
